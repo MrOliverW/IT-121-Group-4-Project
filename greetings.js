1e5f0534c01904c869053d256b1e29306d64a852
Oliver Warlick <warlickoliver@gmail.com>
2024-01-10 15:07:18 -0800

    added comments to code and the required JS file

---
+++ greetings.js
@@ -0,0 +1,5 @@
+/*As per our teacher, Dr.Martins', request we have added a multi-line comment.
+We hope you have enjoyed this multi-line comment.  */
+
+/*Creates an alert using JS that welcomes our teacher.*/
+alert("Hi there, Dr.Martins :) ! It's us: Marcella Touch, Oliver Warlick, Quynh Nguyen, Phuc Nguyen, Isidro Lucatero");
